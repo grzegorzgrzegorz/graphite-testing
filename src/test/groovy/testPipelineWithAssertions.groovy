847eed67f25e7232a994dff767b075c286230c94
Grzegorz <grzegorz@passfailerror.com>
2022-08-20 16:56:49 +0200

    Add test with assertions

---
+++ src/test/groovy/testPipelineWithAssertions.groovy
@@ -0,0 +1,74 @@
+import com.passfailerror.Assertion
+import com.passfailerror.ResultStack
+import com.passfailerror.ResultStackProcessor
+import groovy.test.GroovyTestCase
+
+import java.nio.file.Files
+import java.nio.file.Paths
+
+class testPipelineWithAssertions extends GroovyTestCase {
+
+    Script pipelineScript
+    ResultStackProcessor stackProcessor
+
+    def steps = ["label", "echo", "sh"]
+    def sections = ["pipeline", "agent", "stages", "stage", "steps"]
+
+
+    def mockJenkins(pipelineScript, mockedSteps, mockedSections) {
+        mockSteps(pipelineScript, mockedSteps)
+        mockSections(pipelineScript, mockedSections)
+    }
+
+    def mockSections(pipelineScript, mockedSections) {
+        mockedSections.each{
+            section ->
+                def currentSection = section
+                pipelineScript.metaClass."$currentSection" = { Object... params ->
+                    ResultStackProcessor.getInstance().storeInvocation(currentSection, params, pipelineScript.getBinding().getVariables())
+                    log.info(currentSection)
+                    log.info(params[0]) // steps{closure}
+                    if (params.length > 1) {
+                        log.info(params[1]) // stage("name"){closure}
+                    }
+                }
+
+        }
+    }
+
+
+    def mockSteps(pipelineScript, mockedSteps) {
+        mockedSteps.each {
+            step ->
+                def currentStep = step
+                pipelineScript.metaClass."$currentStep" = { Object... params ->
+                    ResultStackProcessor.getInstance().storeInvocation(currentStep, params, pipelineScript.getBinding().getVariables())
+                    log.info(currentStep + " " + params[0].toString())
+                }
+        }
+    }
+
+
+    void setUp() {
+        def pipelinePath = Paths.get("src/main/groovy/com/passfailerror/simplePipeline.groovy")
+        def pipelineFile = pipelinePath.toFile()
+
+        ResultStackProcessor.setPipelineFile(pipelineFile)
+        ResultStackProcessor.setPipelineFileContent(Files.readAllLines(pipelinePath))
+        ResultStackProcessor.setResultStack(new ResultStack())
+
+        def binding = new Binding()
+        GroovyShell shell = new GroovyShell(binding)
+        pipelineScript = shell.parse(pipelineFile)
+        mockJenkins(pipelineScript, steps, sections)
+
+    }
+
+    void testPipeline() {
+        pipelineScript.run()
+        ResultStackProcessor.getResultStack().print()
+        assert Assertion.stage("First stage").calls("sh")
+        assert !Assertion.stage("First stage").hasEnvVariable("test1")
+    }
+
+}
